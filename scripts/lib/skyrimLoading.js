0c2373dd46c7f30b35db47c9bbbda27a20f11a27
Tonval <valerija.toncinska12@gmail.com>
2026-07-14 11:29:06 +0300

    try another way

--- scripts/lib/skyrimLoading.js
+++ scripts/lib/skyrimLoading.js
@@ -57,10 +57,12 @@ export async function setupSkyrimLoadingTips(active = true) {
 function showLoadingOverlay({ art, text, duration = 5000 }) {
   const existing = document.getElementById("sundry-skyrim-loading-tip");
   if (existing) existing.remove();
-  var riseDistance =
-    art.naturalWidth > art.naturalHeight
-      ? "-10%"
-      : "-20%";
+
+  art.onload = () => {
+  const artRise =
+    art.naturalWidth > img.naturalHeight
+      ? "artRiseLandscape"
+      : "artRisePortrait";
 
   const style = document.createElement("style");
   style.id = "sundry-skyrim-loading-tip-style";
@@ -74,9 +76,19 @@ function showLoadingOverlay({ art, text, duration = 5000 }) {
       object-fit:contain;
       object-position:bottom center;
       z-index:2;
-      rise-distance: ${riseDistance};
-      animation: artRise ${duration}ms;
+      animation: ${artRise} ${duration}ms;
     }
+  
+    @keyframes artRiseLandscape {
+      from { transform: translateY(0); }
+      to   { transform: translateY(-10%); }
+    }
+
+    @keyframes artRisePortrait {
+      from { transform: translateY(0); }
+      to   { transform: translateY(-20%); }
+    }
+
   `;
   document.head.appendChild(style);
 
